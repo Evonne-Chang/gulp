3e34d70f3023b5cbdbcb7da75775709dcc2f8a22
Evonne-Chang <lovelikeangela@hotmail.com>
2022-06-01 13:54:58 +0800

    add

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function aa(x){
+ return x*10 
+}
+console.log(aa(5));
+
+// jquery
+import $ from 'jquery';
+
+import './css/header.css';
+import './css/footer.css';
+import './css/style.css';
+
+
+$('body').css('background-color' , "#010027");

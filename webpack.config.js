4f1e3b5061ef5005411427172b42cba6939350c2
Evonne-Chang <lovelikeangela@hotmail.com>
2022-06-01 14:25:19 +0800

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',               // 入口文件
@@ -9,16 +10,26 @@ module.exports = {
       module: {
         rules: [{
             // 格式
-            test: /\.css$/,
+            test: /\.(sass|scss|css)$/,
             //順序是由下到上 css > style
-            use: [
-                'style-loader',
-                'css-loader'
+            use: [{
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: './dist'
+                }
+              },
+                //'style-loader', 會跟原本的衝突 
+                'css-loader',
+                'sass-loader'
             ],
         }]
 
-    },              // 處裡對應模組
-    //plugins: [],             // 對應的插件
+    },           // 處裡對應模組
+    plugins: [
+      new MiniCssExtractPlugin({
+          filename: "./css/style.css"
+      })
+  ],             // 對應的插件             // 對應的插件
     //devServer: {},           // 服務器配置
     mode: 'development'      // 開發模式配置   //上線用 production
 }
